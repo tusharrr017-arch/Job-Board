b00a4473fc286e766e5fb6d9d83ac183dee4f381
tusharrr017-arch <tusharrr017@gmail.com>
2026-02-17 13:19:20 +0530

    updated

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,17 @@
-
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma =
+  globalForPrisma.prisma ?? createPrismaClient();
 
-export { prisma };
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
